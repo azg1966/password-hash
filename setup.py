1de0dc1a0b53476ea01e2b894355ec83be8fc7f5
alex <alex@localhost>
2020-04-24 11:52:04 +0400

    add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PasswordHash',
+    version='0.1',
+    packages=find_packages(),
+    author='Alexander Z.',
+    description="Simple generate and check password hash functions")
